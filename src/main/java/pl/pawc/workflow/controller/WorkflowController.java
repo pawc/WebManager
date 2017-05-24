c4e411491deb8d62da63b7a6f8ee571f7116e727
pawc <pswid89@gmail.com>
2017-05-24 21:22:20 +0200

    dispatcher servlet

---
+++ src/main/java/pl/pawc/workflow/controller/WorkflowController.java
@@ -0,0 +1,16 @@
+package pl.pawc.workflow.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class WorkflowController{ 
+	@RequestMapping("form")
+    public ModelAndView printHello(HttpServletRequest request, HttpServletResponse response) {   
+		return new ModelAndView("form", "message", "hello mvc");
+    }
+}
